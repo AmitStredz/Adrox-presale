ecd936a25fea5f2f2d789bde5049a11cf3d69a81
AmitStredz <ponnan2112@gmail.com>
2024-07-31 22:18:10 +0530

    Update Adrox.js

--- src/Components/Adrox.js
+++ src/Components/Adrox.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import { useState, Suspense, lazy } from "react";
 
-// import NavBar from './LandingPage/NavBar';
-// import Footer from './LandingPage/Footer';
-// import Content from './LandingPage/Content';
+import NavBar from './LandingPage/NavBar';
+import Footer from './LandingPage/Footer';
+import Content from './LandingPage/Content';
 
-const NavBar = lazy(() => import("./LandingPage/NavBar"));
-const Footer = lazy(() => import("./LandingPage/Footer"));
-const Content = lazy(() => import("./LandingPage/Content"));
+// const NavBar = lazy(() => import("./LandingPage/NavBar"));
+// const Footer = lazy(() => import("./LandingPage/Footer"));
+// const Content = lazy(() => import("./LandingPage/Content"));
 
 function LandingPage() {
   return (
