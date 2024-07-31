2a285546f65d3e7bf81555f13f763281988ede1c
AmitStredz <ponnan2112@gmail.com>
2024-07-31 21:20:57 +0530

    lazy implemented

--- src/Components/Adrox.js
+++ src/Components/Adrox.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import NavBar from './LandingPage/NavBar';
-import Footer from './LandingPage/Footer';
-import Content from './LandingPage/Content';
+import { useState, Suspense, lazy } from "react";
 
+// import NavBar from './LandingPage/NavBar';
+// import Footer from './LandingPage/Footer';
+// import Content from './LandingPage/Content';
+
+const NavBar = lazy(() => import("./LandingPage/NavBar"));
+const Footer = lazy(() => import("./LandingPage/Footer"));
+const Content = lazy(() => import("./LandingPage/Content"));
 
 function LandingPage() {
-    return(
-        <>
-            <NavBar />
-            <Content />
-            <Footer />
-        </>   
-    );
+  return (
+    <>
+      <Suspense fallback='Loading...'>
+        <NavBar />
+      </Suspense>
+      <Suspense fallback='Loading...'>
+        <Content />
+      </Suspense>
+      <Suspense fallback='Loading...'>
+        <Footer />
+      </Suspense>
+    </>
+  );
 }
 
 export default LandingPage;
